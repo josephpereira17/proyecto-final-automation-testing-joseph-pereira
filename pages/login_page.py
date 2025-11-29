6fd96cbd671f0165506d68cda120e4653afc7d57
josephpereira17 <josephpereira1913@gmail.com>
2025-11-29 16:48:21 -0300

    se agregan las dependencias para instalar pytest y las primeras pruebas

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class LoginPage(BasePage):
+    # --- Localizadores ---
+    USERNAME_FIELD = (By.ID, "user-name")
+    PASSWORD_FIELD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    
+    # --- Métodos ---
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def login(self, username, password):
+        """
+        Ejecuta la secuencia completa de inicio de sesión.
+        """
+        self.go_to_url(self.URL)
+
+        # Escribir nombre de usuario y clave
+        self._find_element(*self.USERNAME_FIELD).send_keys(username)
+        self._find_element(*self.PASSWORD_FIELD).send_keys(password)
+        
+        # Hacer clic en el botón de login
+        self._find_element(*self.LOGIN_BUTTON).click()
