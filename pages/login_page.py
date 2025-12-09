8419829c2b1282c3fd12b5bf04c82cc36860bfc6
josephpereira17 <josephpereira1913@gmail.com>
2025-12-08 21:45:25 -0300

    se agregan nuevos tests de front

--- pages/login_page.py
+++ pages/login_page.py
@@ -17,13 +17,11 @@ class LoginPage(BasePage):
         """
         self.go_to_url(self.URL)
 
-        # Escribir nombre de usuario y clave
-        self._find_element(self.USERNAME_FIELD).send_keys(username)
-        self._find_element(self.PASSWORD_FIELD).send_keys(password)
         self._find_element(self.USERNAME_FIELD).clear()
         self._find_element(self.PASSWORD_FIELD).clear()
+        self._find_element(self.USERNAME_FIELD).send_keys(username)
+        self._find_element(self.PASSWORD_FIELD).send_keys(password)
 
-        # Hacer clic en el botón de login
         self._find_element(self.LOGIN_BUTTON).click()
 
         def get_error_message(self) -> str:
